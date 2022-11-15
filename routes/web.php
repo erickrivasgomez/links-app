85c2d2e94c70ad0807a4d29050e5c62657bd092e
Erick Rivas Gomez <rivaserick@outlook.com>
2022-11-15 13:41:00 -0600

    First commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function() {
+
+    Route::get('/links', 'LinkController@index');
+    Route::get('/links/new', 'LinkController@create');
+    Route::post('/links/new', 'LinkController@store');
+    Route::get('/links/{link}', 'LinkController@edit');
+    Route::post('/links/{link}', 'LinkController@update');
+    Route::delete('/links/{link}', 'LinkController@destroy');
+
+    Route::get('/settings', 'UserController@edit');
+    Route::post('/settings', 'UserController@update');
+
+});
+
+Route::post('/visit/{link}', 'VisitController@store');
+
+Route::get('{user}', 'UserController@show');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
